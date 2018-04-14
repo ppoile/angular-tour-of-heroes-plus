67edb29722c26eae5f7d77e04dab742ae89b8a08
Andi Meier <andi@daemeier-clan.ch>
2018-04-14 12:17:49 +0200

    step 4: Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,12 +8,14 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Sturmwind'
-  };
+  heroes = HEROES;
+  selectedHero: Hero = this.heroes[0];
 
   constructor() { }
 
   ngOnInit() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
